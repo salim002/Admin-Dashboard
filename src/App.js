c305ec6ade285c59d304b0957796311fb192790d
MD SALIM <mdsalim.iiitr@gmail.com>
2023-12-02 12:50:33 +0530

    Fectched users list from the provided link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, {useState, useEffect} from "react";
 import './App.css';
+import {getUsers} from "./data/fetchData";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers(setUsers);
+    // console.log(users);
+  }, []);
+  
+
   return (
     <div className="App">
-      Admin Dashboard
+
     </div>
   );
 }
---
+++ src/data/fetchData.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const URL = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+export const addAttributes = (users)=> {
+    return users.map(user => {
+        user.selected = false;
+        user.edit = false;
+        user.show = true;
+        return user;
+    })
+} 
+
+const getUsers = async (setUsers) => {
+  try {
+    const res = await axios.get(URL);
+    // console.log(res.data);
+    const newData = addAttributes(res.data);
+    // console.log(newData);
+    setUsers(newData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {getUsers};
